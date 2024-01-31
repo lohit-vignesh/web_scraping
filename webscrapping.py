5d64a0887962841b3e7a48136bbd13f5ca2419b9
lohit-vignesh <lohitvignesh.narayanswamy@bootlabstech.com>
2024-01-31 16:35:19 +0530

    with contents

--- webscrapping.py
+++ webscrapping.py
@@ -10,8 +10,9 @@ def get_all_a_tags(url):
         for anchor_tag in a_tags:
             href = anchor_tag.get('href')
             if href != None and href != '#':
-                file.write(href+'\n')
-                h_links.append(href)
+                if 'wpriver' not in href:
+                    file.write(href+'\n')
+                    h_links.append(href)
     return h_links
                 
     
@@ -32,33 +33,49 @@ def complete_link(url,list):
     return full_links
 
 
+def get_title(link):
+    page_content = bs4_contents(link)
+    title_tag = page_content.find_all('title')
+    title = title_tag[0]
+    return title
+
+
 def get_content(url):
     body_contents = bs4_contents(url)
     body = body_contents.get_text()
-    with open('content.txt', 'w') as f:
-        f.write(body)
-    return body
+    with open('content.txt', 'a') as f:
+        f.write(str(get_title(url)))
+        if body != None:
+            f.write(body)
 
 
-def get_all_data(url):
+def get_all_links(url):
     visited = []
     atags = get_all_a_tags(url)
     full_links = complete_link(url,atags)
     with open('atags.txt','a') as f:       
         for link in full_links:
-            atwt = get_all_a_tags(link)
-            atwt_full_links = complete_link(link,atags)
-            for at in atwt_full_links:
-                f.write(at+'\n')
-                print(at)
-            
+            if link not in visited:
+                atwt = get_all_a_tags(link)
+                atwt_full_links = complete_link(link,atags)
+                visited.append(link)
+                f.write(link+'\n')
+                get_content(link)
+                for at in atwt_full_links:
+                    if at not in visited:
+                        f.write("   "+at+'\n')
+                        visited.append(at)
+                        get_content(at)                   
+                        # print(at)
+    for i in visited:
+        print(i)
 
 def main():
     # Init All Website links
-    all_links = 'web_url'
+    all_links = 'https://www.bootlabstech.com/'
 
     # Get all Tags from the webpage source
-    get_all_data(all_links)
+    get_all_links(all_links)
 
     # Form Proper URLs based on tags
 
